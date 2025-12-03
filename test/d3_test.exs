d43cbf0d00eb97796d88ba3b699dceca93a88399
James <jamesrobertsongroves@gmail.com>
2025-12-03 01:01:39 +0000

    d3 p1 test

---
+++ test/d3_test.exs
@@ -0,0 +1,11 @@
+defmodule Aoc2025.D3Test do
+  use ExUnit.Case
+
+  test "part 1 example" do
+    assert Aoc2025.D3.p1("inputs/d3ex.txt") == 357
+  end
+
+  # test "part 2 example" do
+  #   assert Aoc2025.D3.p2("inputs/d3ex.txt") == 6
+  # end
+end
